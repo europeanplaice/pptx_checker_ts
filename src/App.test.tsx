51f6a55b56e1e4553b54b1d3da423014861b35ea
Tomohiro Endo <paul.thomas.goodfellas@gmail.com>
2022-09-27 18:29:16 +0900

    Add files via upload

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/PowerPoint Font Checker/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+// test('hellofunc', () => 
+// {
+//   let res: string = hello();
+//   expect(res).toBe("hello");
+// });
