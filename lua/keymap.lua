2b0da242baff0d443a4d62316f99789e0f5da565
Tomohiro Endo <paul.thomas.goodfellas@gmail.com>
2022-10-21 19:06:51 +0900

    Add files via upload

---
+++ lua/keymap.lua
@@ -0,0 +1,8 @@
+
+vim.keymap.set('n', '<C-Z>', 'u', { noremap = true })
+vim.keymap.set('n', '<C-v>', '<C-v>', { noremap = true })
+vim.keymap.set('n', ',p', '0v^h"ayo<esc>"ap"0p', { noremap = true })
+vim.keymap.set('n', ',P', '0v^h"ayO<esc>"ap"0p', { noremap = true })
+vim.keymap.set('n', ',o', 'o<esc>k^j', { noremap = true })
+vim.keymap.set('n', ',O', 'O<esc>j^k', { noremap = true })
+vim.keymap.set('n', 'o', 'zzo', { noremap = true })
